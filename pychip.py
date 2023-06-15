9706eb62dc68a252c87dc6f66b669ed38e5dd393
Andy <andy.gallay@polymtl.ca>
2023-06-15 15:58:20 -0400

    First version of Print_Help function

--- pychip.py
+++ pychip.py
@@ -1,6 +1,47 @@
+cmd_list =[
+    "help",
+    "vars",
+    "cleanVars",
+    "buildCT",
+    "rebuildCT",
+    "startThread",
+    "getThreadDataset",
+    "bleThread",
+    "bleWifi",
+    "on",
+    "off",
+    "toggle",
+    "parsePayload"        
+]
+
+def print_bold(text: str):
+    print('\033[1m' + text + '\033[0m')
+
+def print_green(text: str):
+    print('\033[92m' + text + '\033[0m')
+
 def Print_Help():
-    # TODO
-    None
+    print("This bash script centralize and simplifies the use of chip-tool and starting a clean thread network")
+    print("Here is and overview of the Available cmds :\n")
+
+    for cmd in cmd_list:
+        print(cmd)
+
+    print("\n")
+
+    print_bold("Available options :")
+    print(" -h, --help			Print this help")
+    print(" -n, --nodeId DIGIT		Specify the Nodeid you are trying to reach")
+    print(" -di, --discriminator DIGIT		Specify the discriminator of the device to commission")
+    print(" -e, --endpoint DIGIT		Specify an endpoint to the desired the cluster")
+    print(" -d, --dataset HEX_STRING       Thread Operation Dataset")
+    print(" -s, --ssid STRING		WiFi AP ssid that the end devices needs to connect to")
+    print(" -p, --password STRING		WiFi AP password")
+
+    print_green("Those configurations are held until the device is rebooted !")
+    Print_Vars()
+    print_green("You can also enter the full chip-tool command (without the chip-tool) e.g: Mattertool levelcontrol read current-level 106 1")
+
 
 def Print_Vars():
     # TODO
@@ -46,4 +87,4 @@ def Send_ParseSetupPayload():
     # TODO
     None
 
-print("Hello world")
+Print_Help()

da3023e1abce0e5efc4abaeb35e9202355619049
Andy <andy.gallay@polymtl.ca>
2023-06-15 15:34:56 -0400

    Initial commit with pychip.py script.

---
+++ pychip.py
@@ -0,0 +1,49 @@
+def Print_Help():
+    # TODO
+    None
+
+def Print_Vars():
+    # TODO
+    None
+
+def Clean_Vars():
+    # TODO
+    None
+
+def Clean_build_ChipTool():
+    # TODO
+    None
+
+def Rebuild_ChipTool():
+    # TODO
+    None
+
+def Start_ThreadNetwork():
+    # TODO
+    None
+
+def Get_ThreadDataset():
+    # TODO
+    None
+
+def Pair_BLE_Thread():
+    # TODO
+    None
+
+def Pair_BLE_WiFi():
+    # TODO
+    None
+
+def Send_OnOff_Cmds():
+    # TODO
+    None
+
+def Send_ParseSetupPayload():
+    # TODO
+    None
+
+def Send_ParseSetupPayload():
+    # TODO
+    None
+
+print("Hello world")
